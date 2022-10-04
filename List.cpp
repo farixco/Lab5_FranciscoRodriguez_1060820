24a3972ce0102bf7f2cec51ed4f55fa3bd36655d
farixco <113187191+farixco@users.noreply.github.com>
2022-10-03 19:21:41 -0600

    Definición inicial de List::Add

--- List.cpp
+++ List.cpp
@@ -1,1 +1,17 @@
 #include "List.h"
+void List::Add(int item) {
+	Node* nuevo = new Node;
+	if (header == nullptr) {
+		nuevo->data = item;
+		nuevo->next = nullptr;
+		header = nuevo;
+	}
+	else {
+		nuevo = header;
+		while (nuevo->next != nullptr) {
+			nuevo = nuevo->next;
+		}
+		nuevo->next = nullptr;
+		nuevo->data = item;
+	}
+}
