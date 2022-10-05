846bcc629ec983193fb1664227e25ae871f09d3f
farixco <113187191+farixco@users.noreply.github.com>
2022-10-04 21:44:20 -0600

    Definición inicial de PilaCarta::Push

--- PilaCarta.cpp
+++ PilaCarta.cpp
@@ -1,1 +1,16 @@
 #include "PilaCarta.h"
+void PilaCarta::Push(int num, bool col) {
+	Node* nuevo = new Node;
+	if (header == nullptr) {
+		nuevo->Numero = num;
+		nuevo->Color = col;
+		nuevo->Next = nullptr;
+		header = nuevo;
+	}
+	else {
+		nuevo->Numero = num;
+		nuevo->Color = col;
+		nuevo->Next = header;
+		header = nuevo;
+	}
+}
