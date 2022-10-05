6868b20deb53593c4268526f008a185d7033e44c
farixco <113187191+farixco@users.noreply.github.com>
2022-10-04 23:51:45 -0600

    Barajar modificaicon

--- Mazo.cpp
+++ Mazo.cpp
@@ -17,7 +17,7 @@ void Mazo::Barajar() {
 	}
 
 	PilaCarta* mano = new PilaCarta;
-	for (int i = 0; i < 13; i++) {
+	for (int i = 1; i < 14; i++) {
 		for (int j = 0; j < 4; j++) {
 			if (j % 2 == 0) {
 				mano->Push(i, true);
