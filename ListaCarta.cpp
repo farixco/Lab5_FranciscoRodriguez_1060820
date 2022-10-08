2673b781ff5e8a5bb0cba87e20c2ad83aa905782
farixco <113187191+farixco@users.noreply.github.com>
2022-10-07 22:26:26 -0600

    Definición inicial de ListaCarta::Add

--- ListaCarta.cpp
+++ ListaCarta.cpp
@@ -1,1 +1,19 @@
 #include "ListaCarta.h"
+void ListaCarta::Add(int num, bool col) {
+	Node* nuevo = new Node;
+	if (header == nullptr) {
+		nuevo->Numero = num;
+		nuevo->Color = col;
+		nuevo->Next = nullptr;
+		header = nuevo;
+	}
+	else {
+		nuevo = header;
+		while (nuevo->Next != nullptr) {
+			nuevo = nuevo->Next;
+		}
+		nuevo->Next = nullptr;
+		nuevo->Numero = num;
+		nuevo->Color = col;
+	}
+}
